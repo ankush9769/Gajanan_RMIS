a34b217163787266f79a871edc581464b51b8160
Manishr345 <manishr030405@gmail.com>
2025-03-05 19:00:19 +0530

    Header and Home page created by Hina Hate

--- Gajanan_RMIS_ARTS/src/components/Ankush/Header.jsx
+++ Gajanan_RMIS_ARTS/src/components/Ankush/Header.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap
 
 const Header = () => {
   return (
     <div>
-      this is header
-    </div>
-  )
-}
+      {/* Header with Logo */}
+      <header className="bg-light py-3 text-center">
+        <div className="container">
+          <img src="/home_images/Home Hero Banner.png" alt="Shri Gajanan Arts LLP" className="img-fluid" style={{ height: "60px" }} />
+          <h1 className="fw-bold mt-2">Shri Gajanan Arts LLP</h1>
+        </div>
+      </header>
 
-export default Header
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#6B4A23" }}>
+        <div className="container">
+          <span className="fw-bold text-warning fs-4">卐</span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active text-white fw-semibold mx-3" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white fw-semibold mx-3" href="#">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white fw-semibold mx-3" href="#">Categories</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white fw-semibold mx-3" href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <span className="fw-bold text-warning fs-4">卐</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
